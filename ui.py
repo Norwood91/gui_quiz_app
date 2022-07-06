52b9599fba549127babc25a0b515a9cb3de41184
Robert Norwood <you@example.com>
2022-07-05 18:38:12 -0700

    Got the buttons to work and display the correct choice in the console.

--- ui.py
+++ ui.py
@@ -16,6 +16,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Some Question Text",
             fill=THEME_COLOR,
             font=('Arial', 20, 'italic')
@@ -23,8 +24,8 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
         true_image = PhotoImage(file="images/true.png")
         false_image = PhotoImage(file="images/false.png")
-        self.true_button = Button(image=true_image, highlightthickness=0)
-        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
         self.true_button.grid(row=2, column=0)
         self.false_button.grid(row=2, column=1)
         self.get_next_question()
@@ -34,3 +35,9 @@ class QuizInterface:
     def get_next_question(self):
         q_text = self.quiz.next_question()
         self.canvas.itemconfig(self.question_text, text=q_text)
+
+    def true_pressed(self):
+        self.quiz.check_answer('True')
+
+    def false_pressed(self):
+        self.quiz.check_answer('False')
